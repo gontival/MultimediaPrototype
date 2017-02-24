4df1cc963ace42586306c818bb557f2a60301d44
MCNEXT\jlevant <jlevant@mcnext.com>
2017-02-24 10:53:10 +0100

    a webpage that displays video

---
+++ MultimediaPrototype/Scripts/default.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+$(document).ready(function () {
+    $('#media').on('play', function () {
+        $('#message').html($('#media')[0].currentSrc);
+    });
+});
+
